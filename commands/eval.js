b7976a0b5f4782e7ba567171952b40580d35ab6e
Glitch (dcci-bot) <none>
2018-07-08 21:44:45 +0000

    🕰🏩 Checkpoint ./config.json:1430700/82 ./index.js:1430700/29 ./commands/eval.js:1430700/3458

---
+++ commands/eval.js
@@ -0,0 +1,22 @@
+exports.run = (client, message, args) => {
+  if (message.author.id !== "209635318503047168") return;
+  const argument = message.content.split(" ").slice(1);
+	const clean = text => {
+  	if (typeof(text) === "string")
+    	return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+  	else
+   		return text;
+		}
+		if (message.author.id !== "209635318503047168") return;
+		try {
+			const code = argument.join(" ");
+			let evaled = eval(code);
+			if (typeof evaled !== "string"){
+				evaled = require("util").inspect(evaled);
+				message.channel.send(clean(evaled), {code:"xl"});
+				}
+			} 
+			catch (err) {
+				message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+			}
+}
