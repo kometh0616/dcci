c5f305661b9aa46ab737e10838cea93a8cd6a4c2
Glitch (dcci-bot) <none>
2018-07-09 17:52:47 +0000

    🏙💨 Checkpoint ./commands/help.js:1430700/1394

--- commands/help.js
+++ commands/help.js
@@ -1,33 +1,29 @@
 const fs = require('fs')
 exports.run = (client, message, args) => {
-  fs.readdir("./commands/", (err, files) => {
-		if (err) return console.error(err)
-		let jsfiles = files.filter(f => f.split(".").pop() === "js")
-		if (jsfiles.length <= 0){
-			console.log("No commands to load")
-			return
-		}
-		var namelist = ""
-		var desclist = ""
-		var sublist = ""
-		var usagelist = ""
-    var message = []
-			jsfiles.forEach((f, i) => { 
-				let props = require(`./${f}`)
-				namelist = props.help.name
-				desclist = props.help.description
-				sublist = props.help.subcommands
-				usagelist = props.help.usage
-        message.push(`${namelist} - ${desclist}`)
-			})
-	})
+  var namelist = ""
+	var desclist = ""
+  var helplist = []
   if (!args[0]){
-    message.reply('sending you a DM of my commands!')
-    message.author.createDM().then((dmChannel) => {
-      dmChannel.send(`Here is a list of my commands and their brief descriptions: \n\n${message, {split: true}}\n\n If you want to find out more on an exact command, do \`./help <command>\`!`)
+    fs.readdir("./commands/", (err, files) => {
+	  	if (err) return console.error(err)
+	  	let jsfiles = files.filter(f => f.split(".").pop() === "js")
+	  	if (jsfiles.length <= 0){
+	  		console.log("No commands to load")
+		  	return
+	  	}
+	  	jsfiles.forEach((f, i) => { 
+	  		let props = require(`./${f}`)
+  			namelist = props.help.name
+  			desclist = props.help.description
+        helplist.push(`${namelist} - ${desclist}`)
+	  		})
+	  })
+  message.reply("sent you a list of my commands!")
+  message.author.createDM().then((dmChannel) => {
+  dmChannel.send(helplist, {split: true})
     })
   }
-  else if (args[0]){
+  if (args[0]){
     let command = client.commands.get(args[0]) || client.commands.find(c => c.help.name && c.help.name.includes(args[0]))
     if (!command){
       return message.channel.send(`That is not a valid command!`)
