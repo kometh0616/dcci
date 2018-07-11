6337e6594f5c967bbf719d68ff728b88e0e0adbe
Glitch (dcci-bot) <none>
2018-07-11 11:59:17 +0000

    📻🌮 Checkpoint ./events/ready.js:1430700/19

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,7 @@ module.exports = (client) => {
   client.user.setPresence({
 		game: {
 			type: 'WATCHING',
-			name: 'over DCCI | Do ./help!'
+			name: 'over DCCI | Do >help!'
 		},
 		status: 'online'
 	})
