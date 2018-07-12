5ba6a0730a7ce524ef6be1acf60a14fb2d2fe145
Glitch (dcci-bot) <none>
2018-07-12 21:41:26 +0000

    👍🚃 Checkpoint ./commands/ping.js:1430700/26

--- commands/ping.js
+++ commands/ping.js
@@ -13,9 +13,9 @@ exports.run = async (client, message, args) => {
 	}
 	let thisMuch = await findInDatab.get('amount')
 	findInDatab.increment('amount').catch(console.error)
-	message.channel.send(`Pong! This command was called ${thisMuch} times!\nYour ping is: \`${Date.now() - message.createdAt}\`ms!`).catch(console.error)
+	message.channel.send(`Pong! This command was called ${thisMuch} times!\nYour ping is: \`${Date.now() - . }\`ms!`).catch(console.error)
 }
-
+;
 exports.help = {
 	name: 'ping',
 	description: 'Tells you your ping.',
