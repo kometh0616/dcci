4bc1034f901eb54ad101b40a556d2ba41e565521
Glitch (dcci-bot) <none>
2018-07-17 20:00:15 +0000

    🚝🌊 Checkpoint ./index.js:1430700/6

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ fs.readdir("./events/", (err, files) => {
 	files.forEach(file => {
 		const event = require(`./events/${file}`);
 		let eventName = file.split(".")[0]
-		client.on(eventName, event.bind(null, client, xlsx))
+		client.on(eventName, event.bind(null, client))
 	})
 })
 
