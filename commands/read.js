80ec39e8ae38a06978649b44883138d7dce3d5f7
Glitch (dcci-bot) <none>
2018-07-18 10:01:36 +0000

    🐪💑 Checkpoint ./commands/read.js:1430700/109

--- commands/read.js
+++ commands/read.js
@@ -4,6 +4,8 @@ exports.run = (client, message, args) => {
   if (message.author.id !== client.config.botOwnerID) return
   try {
     let workbook = xlsx.readFile('test.xlsx')
+    let aoa = [['Hello', 'World!'], ['Ping', 'Pong'], ['Foo', 'Bar']]
+    workbook.write
     return message.channel.send('Worked!')
   }
   catch (error) {
