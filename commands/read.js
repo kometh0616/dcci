7c99e21645ca0c371e5a0055467d859ae91dbbd8
Glitch (dcci-bot) <none>
2018-07-18 11:57:04 +0000

    🙌🍭 Checkpoint ./commands/read.js:1430700/240

--- commands/read.js
+++ commands/read.js
@@ -3,14 +3,14 @@ const xlsx = require('xlsx')
 exports.run = (client, message, args) => {
   if (message.author.id !== client.config.botOwnerID) return
   try {
-    let workbook = xlsx.readFile('test.xlsx')
+    let workbook = xlsx.utils.book_new()
     let aoa_data = [['Hello', 'World!'], ['Ping', 'Pong'], ['Foo', 'Bar']]
     let name = 'sheetjs'
     let aoa = xlsx.utils.aoa_to_sheet(aoa_data)
     let sheet = xlsx.utils.book_append_sheet(workbook, aoa, name)
     return message.channel.send({
       files: [{
-        attachment: `./test.xlsx`,
+        attachment: `./${name}.xlsx`,
         name: `${name}.xlsx`
       }]
     })
