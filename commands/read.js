ca56e5384111c0bcbb1e832bff9cc617f3109f07
Glitch (dcci-bot) <none>
2018-07-18 08:07:27 +0000

    🌔😾 Checkpoint ./commands/read.js:1430700/357

--- commands/read.js
+++ commands/read.js
@@ -1,12 +1,15 @@
 const fs = require('fs')
 const xlsx = require('xlsx')
+const request = require('request')
 exports.run = (client, message, args) => {
   if (message.author.id !== client.config.botOwnerID) return
   try {
-    xlsx.readFile('test.xlsx')
-    return message.channel.send('Worked!')
+    request(url, {encoding: null}, (err, res, data) => {
+      if (err || res.statusCode !== 200) return console.error(err)
+      var 
   }
   catch (error) {
     console.error(error)
-    return message.channel.send(
+    return message.channel.send('Error!')
+  }
 }
