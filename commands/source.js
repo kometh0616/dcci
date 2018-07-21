6ae20132edc1d78022ba8a583d977cf726beec89
Glitch (dcci-bot) <none>
2018-07-21 13:12:09 +0000

    🎋💆 Checkpoint ./commands/source.js:1430700/66

--- commands/source.js
+++ commands/source.js
@@ -1,2 +1,3 @@
 exports.run = (client, message, args) => {
-  message.channel.send('Here is my source code:https://github.com/kometh0616/dcci
+  message.channel.send('Here is my source code: https://github.com/kometh0616/dcci\nDon\'t tell my developer...')
+}
