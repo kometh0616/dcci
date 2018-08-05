9ff62b3bd6efc4ce4620be84a5361e8e09ad3f40
Glitch (dcci-bot) <none>
2018-08-05 21:36:05 +0000

    🌞🔫 Checkpoint ./commands/update.js:1430700/2350

--- commands/update.js
+++ commands/update.js
@@ -1,36 +1,39 @@
 exports.run = async (client, message, args) => {
   if (!client.guilds.get("320659280686743602").members.get(message.author.id).hasPermission('ADMINISTRATOR')) return
-	var allServers = await DCCIServers.findAll({
-		attributes: ['guildID']
-	})
-	var outerArray = Array.from(allServers.map(a => a.guildID))
-	var innerArray = Array.from(allServers.map(a => a.guildID))
-	outerArray.forEach(async serverID => {
-		var browseDatab = await DCCIServers.findOne({
-			where: {
-				guildID: serverID,
-			}
-		})
-		var eColor = client.guilds.get(serverID).members.get(client.user.id).displayColor
-		var IDfromDatab = browseDatab.get('portalChannel')
-		var portal = client.channels.get(IDfromDatab)
-		portal.bulkDelete(outerArray.length)
-		innerArray.forEach(async ofInfo => {
-			let reBrowse = await DCCIServers.findOne({
-				where: {
-					guildID: ofInfo,
-				}
-			})
-			portal.send({embed: {
-				color: eColor,
-				author: {
-					name: `${reBrowse.get('name')}`,
-					icon_url: client.guilds.get(ofInfo).iconURL
-				},
-				description: `${reBrowse.get('description')}\n\nLink to the server:\n${reBrowse.get('link')}`
-			}})
-		})
-	})
+  async function update() {
+    var allServers = await DCCIServers.findAll({
+		  attributes: ['guildID']
+	  })
+	  var outerArray = Array.from(allServers.map(a => a.guildID))
+	  var innerArray = Array.from(allServers.map(a => a.guildID))
+	  outerArray.forEach(async serverID => {
+	  	var browseDatab = await DCCIServers.findOne({
+	  		where: {
+	  			guildID: serverID,
+	  		}
+	  	})
+	    var eColor = client.guilds.get(serverID).members.get(client.user.id).displayColor
+		  var IDfromDatab = browseDatab.get('portalChannel')
+		  var portal = client.channels.get(IDfromDatab)
+		  portal.bulkDelete(outerArray.length)
+		  innerArray.forEach(async ofInfo => {
+		  	let reBrowse = await DCCIServers.findOne({
+		  		where: {
+		  			guildID: ofInfo,
+		  		}
+		  	})
+		  	portal.send({embed: {
+		  		color: eColor,
+		  		author: {
+		  			name: `${reBrowse.get('name')}`,
+		  			icon_url: client.guilds.get(ofInfo).iconURL
+		  		},
+		  		description: `${reBrowse.get('description')}\n\nLink to the server:\n${reBrowse.get('link')}`
+		  	}})
+	  	})
+	  })
+  }
+  setInterval(update, 900000)
 	return message.channel.send("Done!")
 }
 
