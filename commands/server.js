335270b134c460ad12142fc8ace772ded6c73bdd
Glitch (dcci-bot) <none>
2018-08-06 17:57:10 +0000

    💕🐴 Checkpoint ./commands/server.js:1430700/1065

--- commands/server.js
+++ commands/server.js
@@ -4,6 +4,18 @@ exports.run = async (client, message, args) => {
 		const servers = await DCCIServers.findAll({
 			attributes: ['guildID', 'name', 'description', 'link']
 		})
-		console.log(servers)
+		var x = 0
+		var curPage = 1
+		var allPages = servers.length
+		var server = await client.guilds.get(servers[x].dataValues.guildID)
+		var embed = new RichEmbed()
+		.setAuthor(servers[x].dataValues.name)
+    .setColor(message.member.displayColor)
+		.setThumbnail(server.iconURL)
+		.setDescription(servers[x].dataValues.description)
+		.addField('Link to the server:', servers[x].dataValues.link)
+		.setFooter(`Page ${curPage}/${allPages} | Requested by ${message.author.tag}`, client.user.avatarURL)
+    .setTimestamp()
+		await message.channel.send({embed})
 	}
 }
