cf0bde14caa58cf260990d28d93cfd0d914ddad4
Glitch (dcci-bot) <none>
2018-08-07 18:45:20 +0000

    🚌🔪 Checkpoint ./watch.json:1430700/26 ./commands/update.js:1430700/15

--- commands/update.js
+++ commands/update.js
@@ -33,7 +33,7 @@ exports.run = async (client, message, args) => {
 	  	})
 	  })
   }
-  setInterval(update, 900000)
+  setInterval(update, 604800000)
   update()
 	return message.channel.send("Done!")
 }
