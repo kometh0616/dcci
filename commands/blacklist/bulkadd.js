00d742088d7ea14f13bbfb23af650bb87bc700a2
Glitch (dcci-bot) <none>
2018-08-12 19:01:39 +0000

    🚨👏 Checkpoint ./commands/blacklist/bulkadd.js:1430700/3787

--- commands/blacklist/bulkadd.js
+++ commands/blacklist/bulkadd.js
@@ -1,43 +1,73 @@
 exports.run = async (client, message, args, Discord, thisServerID, fetchBList, blacklisted, reasonForBList, checkBList, blacklister) => {
-    if (!args[1]) return message.reply('no ID defined!')
-    if (!checkBList){
-        return message.reply(`this user is not blacklisted!`)
-    }
-    else if (checkBList){
-        let getReason = checkBList.get('reason') || "none"
-        client.fetchUser(checkBList.get('userID')).then(user => {
-          message.channel.send({embed:{
-              color: message.member.displayColor,
-              author: {
-                  name: message.author.tag,
-                  icon_url: message.author.avatarURL
-              },
-              title: "Information on blacklisted ID",
-              fields:[{
-                  name: "Blacklisted user:",
-                  value: `<@${checkBList.get('userID')}>\n${user.tag}`
-              },
-              {
-                  name: "ID:",
-                  value: `${checkBList.get('userID')}`
-              },
-              {
-                  name: "Blacklisted by:",
-                  value: `${checkBList.get('author')}`
-              },
-              {
-                  name: "Reason:",
-                  value: `${getReason}`
-              }],
-              timestamp: new Date(),
-              footer: {
-                  icon_url: client.user.avatarURL,
-                  text: "© DCCI Bot"
-              }
-          }})
-  }).catch(err => {
-    console.log(err)
-    message.reply('an error has occured. Chances are the blacklisted user you\'ve declared is not in Discord anymore. If that is not the case, please contact a developer.')
-  })
-    }
+    		var blackIDs = []
+		var included = []
+		for (let arg of args) {
+			if (arg === 'bulkadd' || isNaN(arg) === true) continue
+			let checkIDs = await Blacklist.findOne({
+				where: {
+					userID: arg
+				}
+			})
+			if (checkIDs) {
+				included.push(checkIDs.dataValues.userID)
+				continue
+			}
+			blackIDs.push(arg)
+		}
+		async function perform() {
+			blackIDs.forEach(async id => {
+				async function addToList() {
+					client.fetchUser(id).then(async user => {
+						let reasonForBList = args.slice(blackIDs.length + 1).join(' ') || "None"
+						await Blacklist.create({
+							userID: user.id,
+							tag: user.tag,
+							reason: reasonForBList,
+							author: blacklister
+						})
+						await client.channels.get(client.config.logChannelID).send({embed: {
+							color: client.channels.get(client.config.logChannelID).guild.members.get(client.user.id).displayColor,
+							author: {
+								name: message.author.username,
+								icon_url: message.author.avatarURL
+							},
+							title: "New user added to blacklist!",
+							fields: [{
+								name: "Action performed by:",
+								value: blacklister
+							},
+							 {
+							name: "Blacklisted user:",
+							value: `<@${id}>\n${user.tag}`
+							},
+							{
+								name: "Blacklisted ID:",
+								value: id
+							},
+							{
+								name: "Reason for adding to blacklist:",
+								value: reasonForBList
+							}],
+							timestamp: new Date(),
+							footer: {
+								icon_url: client.user.avatarURL,
+								text: client.config.copymark
+							}
+						}})
+					}).catch(async err => {
+						console.error(err)
+						if (err.code = 10013) await message.channel.send(`User with ID ${id} doesn't exist, skipping...`)
+					})
+				}
+			})
+		}
+		await perform()
+		async function answer() {
+			let reply = 'ID\'s added to blacklist succesfully!'
+			if (included[0] !== undefined) {
+				reply += `\nThese ID's were skipped because they were already in a blacklist:\n${included.join('\n')}`
+			}
+			await message.reply(reply)
+		}
+		await answer()
 }
