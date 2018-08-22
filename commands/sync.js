a61721d27bb07913f83e19dea5719e9c5226832a
Glitch (dcci-bot) <none>
2018-08-22 14:48:21 +0000

    👩‍👩‍👦💒 Checkpoint ./commands/sync.js:1430700/660

--- commands/sync.js
+++ commands/sync.js
@@ -1,3 +1,12 @@
 exports.run = async (client, message, args) => {
+  if (message.author.id !== client.config.botOwnerID) return
+  if (!args[0]) return message.reply('no Sequelize model defined.')
+  let modelName = args[0]
   const { readdir } = require('fs')
+  const { sequelize } = require(`../index.js`)
+  readdir('../models/', (err, files) => {
+    if (!err) return console.error(err)
+    let model = sequelize.import(`../models/${modelName}`)
+    model.sync().then(() => message.reply('model ${modelName} synced succesfully!'))
+  })
 }
