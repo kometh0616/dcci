763e76f74de1f9b75dcae5df41b4b967a2584142
Glitch (dcci-bot) <none>
2018-08-22 14:37:54 +0000

    🦄🌶 Checkpoint ./commands/reload.js:1430700/903 ./commands/sync.js:1430700/128

--- commands/reload.js
+++ commands/reload.js
@@ -1,9 +1,17 @@
 exports.run = (client, message, args) => {
+  const {readdir} = require('fs')
   if (message.author.id !== client.config.botOwnerID) return
   if(!args || args.size < 1) return message.reply("Must provide a command name to reload.");
   const commandName = args[0];
   if(!client.commands.has(commandName)) {
-    
+    readdir(`./`, (err, files) => {
+      if (err) return console.error(err)
+      let command = files.find(x => x.startsWith(commandName))
+      if (!command) return message.reply('That command doesn\'t exist.')
+      const props = require(`./${commandName}.js`)
+      client.commands.set(command, props)
+      return message.reply(`The command ${command} has been loaded`)
+    })
   }
   delete require.cache[require.resolve(`./${commandName}.js`)];
   client.commands.delete(commandName);
---
+++ commands/sync.js
@@ -0,0 +1,3 @@
+exports.run = async (client, message, args) => {
+  const { readdir } = require('fs')
+}
