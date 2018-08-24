0603c2b78a260f45ca2bcc8b453eceac78c24140
Glitch (dcci-bot) <none>
2018-08-24 08:24:44 +0000

    🐲🔪 Checkpoint ./commands/update.js:1430700/4034

--- commands/update.js
+++ commands/update.js
@@ -1,37 +1,74 @@
 exports.run = async (client, message, args) => {
   if (!client.guilds.get("320659280686743602").members.get(message.author.id).hasPermission('ADMINISTRATOR')) return
-    var allServers = await DCCIServers.findAll({
-		  attributes: ['guildID']
-	  })
-	  var outerArray = Array.from(allServers.map(a => a.guildID))
-	  var innerArray = Array.from(allServers.map(a => a.guildID))
-	  outerArray.forEach(async serverID => {
-	  	var browseDatab = await DCCIServers.findOne({
-	  		where: {
-	  			guildID: serverID,
-	  		}
-	  	})
-	    var eColor = client.guilds.get(serverID).members.get(client.user.id).displayColor
-		  var IDfromDatab = browseDatab.get('portalChannel')
-		  var portal = client.channels.get(IDfromDatab)
-		  portal.fetchMessages({limit: outerArray.length}).then(collection => collection.forEach(async message =>  await message.delete()))
-		  innerArray.forEach(async ofInfo => {
-		  	let reBrowse = await DCCIServers.findOne({
-		  		where: {
-		  			guildID: ofInfo,
-		  		}
-		  	})
-		  	await portal.send({embed: {
-		  		color: eColor,
-		  		author: {
-		  			name: `${reBrowse.get('name')}`,
-		  			icon_url: client.guilds.get(ofInfo).iconURL
-		  		},
-		  		description: `${reBrowse.get('description')}\n\nLink to the server:\n${reBrowse.get('link')}`
-		  	}})
-	  	})
-	  })
-	return message.channel.send("Done!")
+	var allServers = await DCCIServers.findAll({
+		attributes: ['guildID']
+	})
+  var allSatellites = await DCCISatellite.findAll({
+    attributes: ['guildID']
+  })
+	var outerArray = Array.from(allServers.map(a => a.guildID))
+	var innerArray = Array.from(allServers.map(a => a.guildID))
+  var outerSat = Array.from(allSatellites.map(a => a.guildID))
+  var innerSat = Array.from(allSatellites.map(a => a.guildID))
+  try {
+    outerArray.forEach(async serverID => {
+      var browseDatab = await DCCIServers.findOne({
+        where: {
+          guildID: serverID,
+        }
+      })
+      var eColor = client.guilds.get(serverID).members.get(client.user.id).displayColor
+      var IDfromDatab = browseDatab.get('portalChannel')
+      var portal = client.channels.get(IDfromDatab)
+      portal.fetchMessages({limit: outerArray.length}).then(collection => collection.forEach(message => message.delete()))
+      innerArray.forEach(async ofInfo => {
+        let reBrowse = await DCCIServers.findOne({
+          where: {
+            guildID: ofInfo,
+          }
+        })
+        await portal.send({embed: {
+          color: eColor,
+          author: {
+            name: `${reBrowse.get('name')}`,
+            icon_url: client.guilds.get(ofInfo).iconURL
+          },
+          description: `${reBrowse.get('description')}\n\nLink to the server:\n${reBrowse.get('link')}`,
+        }})
+      })
+    })
+  } catch (e) {
+    console.log(e)
+    return message.channel.send('Something went wrong. Incident is logged.')
+  } finally {
+    outerSat.forEach(async serverID => {
+      var browseDatab = await DCCISatellite.findOne({
+        where: {
+          guildID: serverID,
+        }
+      })
+      var eColor = client.guilds.get(serverID).members.get(client.user.id).displayColor
+      var IDfromDatab = browseDatab.get('portalChannel')
+      var portal = client.channels.get(IDfromDatab)
+      portal.fetchMessages({limit: outerArray.length}).then(collection => collection.forEach(async message => await message.delete()))
+      innerSat.forEach(async ofInfo => {
+        let reBrowse = await DCCISatellite.findOne({
+          where: {
+            guildID: ofInfo,
+          }
+        })
+        await portal.send({embed: {
+          color: eColor,
+          author: {
+            name: `${reBrowse.get('name')}`,
+            icon_url: client.guilds.get(ofInfo).iconURL
+          },
+          description: `${reBrowse.get('description')}\n\nLink to the server:\n${reBrowse.get('link')}`,
+        }})
+      })
+    })
+    return message.channel.send("Done!")
+  }
 }
 
 exports.help = {
