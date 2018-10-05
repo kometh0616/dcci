3a5adaeb8b95e93f828e085e511116c8efbec100
Glitch (dcci-bot) <none>
2018-10-05 15:28:49 +0000

    💿🎐 Checkpoint ./commands/eval.js:1430700/8

--- commands/eval.js
+++ commands/eval.js
@@ -1,21 +1,21 @@
 exports.run = (client, message, args) => {
-  if (message.author.id !== client.config.botOwnerID){
+  if (message.author.id !== client.config.botOwnerID) {
     return message.channel.send({
       files: ['https://cdn.discordapp.com/attachments/354515583464898572/469992679435010048/SOD_OFF.png']
     }) 
-  };
-  const argument = message.content.split(" ").slice(1);
+  }
+  const argument = message.content.split(" ").slice(1)
 	const clean = text => {
   	if (typeof(text) === "string")
-    	return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+    	return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203))
   	else
-   		return text;
+   		return text
 		}
 		try {
-			const code = argument.join(" ");
-			let evaled = eval(code);
+			const code = argument.join(" ")
+			let evaled = eval(code)
 			if (typeof evaled !== "string"){
-				evaled = require("util").inspect(evaled);
+				evaled = require("util").inspect(evaled)
         message.react('✅').then(message.channel.send(clean(evaled), {code:"xl", split: true}))
 				}
 			} 
