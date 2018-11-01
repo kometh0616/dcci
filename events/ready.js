2095612e41a6278d31fa08da9a6d547b1c7f2eac
Glitch (dcci-bot) <none>
2018-11-01 19:06:05 +0000

    👨‍👨‍👦🐌 Checkpoint ./events/ready.js:1430700/244

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,11 @@
-module.exports = (client) => {
-  client.user.setPresence({
+module.exports = async (client) => {
+  const logChannel = client.channels.get(client.config.logChannelID)
+  await client.user.setPresence({
 		game: {
 			type: 'WATCHING',
 			name: 'over DCCI | Do >help!'
 		},
 		status: 'online'
 	})
+  await logChannel.send(`Logged in as ${client.user.tag} at `)
 }
